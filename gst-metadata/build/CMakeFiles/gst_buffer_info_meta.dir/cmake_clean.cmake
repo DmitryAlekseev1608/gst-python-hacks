5247a8c7f73fb6a70e88c5cc855d5d031f71c0d9
Dmitry <a89652251224@gmail.com>
2024-03-19 17:16:50 +0300

    init

---
+++ gst-metadata/build/CMakeFiles/gst_buffer_info_meta.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gst_buffer_info_meta.dir/gst_buffer_info_meta.c.o"
+  "CMakeFiles/gst_buffer_info_meta.dir/gst_buffer_info_meta.c.o.d"
+  "libgst_buffer_info_meta.pdb"
+  "libgst_buffer_info_meta.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gst_buffer_info_meta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gst-metadata/build/CMakeFiles/gst_buffer_info_meta.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gst_buffer_info_meta.
+# This may be replaced when dependencies are built.
